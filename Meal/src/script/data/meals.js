ff433b0a94145f095ab831f81a10707b8e3fbb2f
Shine Suryadi Tanjung <67227187+shinesuryadi21@users.noreply.github.com>
2020-08-01 19:51:11 +0700

    Belajar Fundamental Front-End Web Development
     Belajar Fundamental Front-End Web Development

---
+++ Meal/src/script/data/meals.js
@@ -0,0 +1,24 @@
+const meals = [
+    {
+        name: "Chiken",
+        fanArt: "https://www.themealdb.com/images/ingredients/Chicken.png",
+        description: "The chicken is a type of domesticated fowl, a subspecies of the red junglefowl (Gallus gallus). It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011. There are more chickens in the world than any other bird or domesticated fowl. Humans keep chickens primarily as a source of food (consuming both their meat and eggs) and, less commonly, as pets. Originally raised for cockfighting or for special ceremonies, chickens were not kept for food until the Hellenistic period (4th–2nd centuries BC). Genetic studies have pointed to multiple maternal origins in South Asia, Southeast Asia, and East Asia, but with the clade found in the Americas, Europe, the Middle East and Africa originating in the Indian subcontinent. From ancient India, the domesticated chicken spread to Lydia in western Asia Minor, and to Greece by the 5th century BC."
+    },
+    {
+        name: "Salmon",
+        fanArt: "https://www.themealdb.com/images/ingredients/Salmon.png",
+        description: "Salmon is the common name for several species of ray-finned fish in the family Salmonidae. Other fish in the same family include trout, char, grayling and whitefish. Salmon are native to tributaries of the North Atlantic (genus Salmo) and Pacific Ocean (genus Oncorhynchus). Many species of salmon have been introduced into non-native environments such as the Great Lakes of North America and Patagonia in South America. Salmon are intensively farmed in many parts of the world. Typically, salmon are anadromous: they hatch in fresh water, migrate to the ocean, then return to fresh water to reproduce. "
+    },
+    {
+        name: "Pork",
+        fanArt: "https://www.themealdb.com/images/ingredients/Pork.png",
+        description: "Pork is the culinary name for the flesh of a domestic pig (Sus scrofa domesticus). It is the most commonly consumed meat worldwide,[1] with evidence of pig husbandry dating back to 5000 BC. Pork is eaten both freshly cooked and preserved. Curing extends the shelf life of the pork products. Ham, smoked pork, gammon, bacon and sausage are examples of preserved pork. ",
+    },
+    {
+        name: "Avocado",
+        fanArt: "https://www.themealdb.com/images/ingredients/Avocado.png",
+        description: "The avocado, a tree with probable origin in South Central Mexico, is classified as a member of the flowering plant family Lauraceae. The fruit of the plant, also called an avocado (or avocado pear or alligator pear), is botanically a large berry containing a single large seed. Avocados are commercially valuable and are cultivated in tropical and Mediterranean climates throughout the world. They have a green-skinned, fleshy body that may be pear-shaped, egg-shaped, or spherical. Commercially, ",
+    }
+];
+
+export default meals;
